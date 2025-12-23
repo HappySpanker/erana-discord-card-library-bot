7ba4e6d8d8887bd44b1d33c765b65699eb8d8d2c
Happy Spanker <happyspanker@outlook.com>
2025-12-23 22:45:34 +0100

    Cleanup scripts + basic Discord interactions

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,37 @@
 import { Client, Events, GatewayIntentBits } from "discord.js";
 import { env } from "node:process";
 
+if (!process.env.DISCORD_TOKEN) {
+  throw new Error("Discord token expected in environment variables yet not found.")
+}
+
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({   intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.DirectMessages
+  ] 
+});
 
 // When the client is ready, run this code (only once).
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
 // It makes some properties non-nullable.
-client.once(Events.ClientReady, (readyClient: Client<true>) => {
+client.once(Events.ClientReady, async (readyClient: Client<true>) =>  {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
+client.on(Events.MessageCreate, (message) => {
+  if (!message.inGuild()) return;
+
+  console.log("message:", {
+      guild: message.guild?.name,
+      channe: message.channel.name,
+      author: message.author.username,
+      content: message.content
+    }
+  );
+});
+
 // Log in to Discord with your client's token
-console.log(env.DISCORD_TOKEN);
-client.login(env.DISCORD_TOKEN);
+await client.login(env.DISCORD_TOKEN);
