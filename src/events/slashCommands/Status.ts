bdd182a287804726d9af12d82bb5c3c094118ebd
Happy Spanker <happyspanker@outlook.com>
2025-12-24 23:55:48 +0100

    Added mock embed output to Status

--- src/events/slashCommands/Status.ts
+++ src/events/slashCommands/Status.ts
@@ -1,5 +1,15 @@
-import { CacheType, ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { CacheType, ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder } from "discord.js";
 import { ISlashCommandHandler } from "./interfaces/ICommandHandler.js";
+
+export type StatusValue = {
+    Erana: {
+        Uptime: string
+    },
+    System: {
+        Uptime: string,
+        Hostname: string
+    }
+}
 
 export const StatusSlashCommandBuilder =
     new SlashCommandBuilder()
@@ -10,12 +20,47 @@ export const StatusSlashCommandBuilder =
  * The command handler for the "Status" slash command
  */
 export class StatusSlashCommandHandler implements ISlashCommandHandler {
-    async handle(interaction: ChatInputCommandInteraction<CacheType>): Promise<void> {
+    public async handle(interaction: ChatInputCommandInteraction<CacheType>): Promise<void> {
         await interaction.deferReply();
 
-        // TODO: get status here?
+        // Get status from backend; for now...
+        const statusValue: StatusValue = {
+            Erana: {
+                Uptime: "234 years"
+            },
+            System: {
+                Uptime: "345 years",
+                Hostname: "localhost"
+            }
+        }
 
-        await interaction.editReply("up!");
+        await interaction.editReply({
+            embeds: [this.statusEmbedBuilder(statusValue)]
+        })
+    }
+
+    private statusEmbedBuilder(statusValue: StatusValue): EmbedBuilder {
+        return new EmbedBuilder()
+            .setTitle("Erana status")
+            .setDescription("Operation status of Erana")
+            .addFields({
+                name: "🧝‍♀️ Erana",
+                value: [
+                    `Uptime: ${statusValue.Erana.Uptime}`
+                ].join("\n"),
+                inline: true
+            })
+            .addFields({
+                name: "🖥️ System",
+                value: [
+                    `Uptime: ${statusValue.System.Uptime}`,
+                    `Hostname: ${statusValue.System.Hostname}`
+                ].join("\n"),
+                inline: true
+            })
+            .setFooter({
+                text: "woot!"
+            })
     }
 
 }
