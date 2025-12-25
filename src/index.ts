be8f05e60fd48445f6e7d738f9de39ccd66334a3
Happy Spanker <happyspanker@outlook.com>
2025-12-25 19:56:15 +0100

    Add error handling for uncaught exceptions and unhandled rejections

--- src/index.ts
+++ src/index.ts
@@ -32,9 +32,38 @@ process.on("SIGINT", () => {
   process.exit(0);
 })
 
+/**
+ * Handle uncaught exceptions/errors
+ */
+process.on("uncaughtException", (error: Error) => {
+  if (isShuttingDown)
+    return;
+
+  isShuttingDown = true;
+  console.error(`Uncaught exception: '${error.name}'; shutting down.`);
+
+  process.exit(1);
+})
+
+/**
+ * Handle unhandled rejections
+ */
+process.on("unhandledRejection", (reason: unknown) => {
+  console.error(`Unhandled rejection: '${reason}';continuing...`);
+})
+
+/**
+ * Handle attaching to already rejecte promise
+ */
+process.on("rejectionHandled", () => {
+  console.warn("Late promise handler attached");
+});
+
 if (!process.env.DISCORD_TOKEN) {
   throw new Error("Discord token expected in environment variables but not found.")
 }
+
+throw new Error("test");
 
 const discordClient = new DiscordClient()
 
