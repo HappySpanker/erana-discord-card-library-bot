25574c7de1ee41e318727a09c392db421c25f43c
Happy Spanker <happyspanker@outlook.com>
2025-12-25 19:27:40 +0100

    Add graceful shutdown handling for SIGTERM and SIGINT signals

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,37 @@
 import { DiscordClient } from "./events/DiscordClient.js";
 
+let isShuttingDown = false;
+
+/**
+ * Handle SIGTERM event
+ */
+process.on("SIGTERM", () => {
+  if (isShuttingDown) 
+    return; // No need
+  
+  isShuttingDown = true;
+  console.log("Received SIGTERM, gracefully shutting down...");
+
+  // Shut down externals here
+  console.log("Shut down gracefully; good night sweet prince.")
+
+  process.exit(0);
+})
+
+/**
+ * Handle SIGINT event
+ */
+process.on("SIGINT", () => {
+  if (isShuttingDown)
+    return; // No need
+
+  isShuttingDown = true;
+
+  console.log("Received SIGINT, shutting down now...");
+ 
+  process.exit(0);
+})
+
 if (!process.env.DISCORD_TOKEN) {
   throw new Error("Discord token expected in environment variables but not found.")
 }
