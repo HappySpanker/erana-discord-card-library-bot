678ecc94e529ceda3473dafdda94972d168e5214
Happy Spanker <happyspanker@outlook.com>
2025-12-30 00:20:28 +0100

    Add Temporal polyfill, WIP on Cards + Characterbooks + orchestrator

---
+++ src/orchestration/models/Cards.ts
@@ -0,0 +1,46 @@
+/**
+ * Based on: https://github.com/malfoyslastname/character-card-spec-v2
+ */
+
+import { Temporal } from "@js-temporal/polyfill"
+import { CharacterBook } from "./Characterbooks.js"
+
+/**
+ * Ancient-but-still-common V2 card spec
+ */
+export type TavernCardV2 = {
+  spec: 'chara_card_v2'
+  spec_version: '2.0' // May 8th addition (refers to may 2023)
+  data: {
+    name: string
+    description: string
+    personality: string
+    scenario: string
+    first_mes: string
+    mes_example: string
+
+    // New fields start here
+    creator_notes: string
+    system_prompt: string
+    post_history_instructions: string
+    alternate_greetings: Array<string>
+    character_book?: CharacterBook
+
+    // May 8th additions (refers to may 2023)
+    tags: Array<string>
+    creator: string
+    character_version: string
+    extensions: Record<string, any>
+  }
+}
+
+/**
+ * This is where the fun begin!
+ * A container for TavenCard specs. All supported versions.
+ */
+export type CardContainer = {
+  userId: string,
+  card: TavernCardV2,
+  created: Temporal.Instant,
+  updated: Temporal.Instant,
+}
