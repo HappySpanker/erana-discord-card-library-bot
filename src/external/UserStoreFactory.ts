ad838e02de0b04d01a869571c6973a8e7de9684e
Happy Spanker <happyspanker@outlook.com>
2026-01-16 21:18:43 +0100

    Implement GetUserFromCanonicalUserId

--- src/external/UserStoreFactory.ts
+++ src/external/UserStoreFactory.ts
@@ -1,21 +1,45 @@
 import { Pool } from "pg";
+import { logger } from "../logger.js";
 
 export interface IUserStore {
-    GetUserIdFromCanonicalUserId(canonicalUserId: string): Promise<string>
+  GetUserFromCanonicalUserId(canonicalUserId: string): Promise<UserDTO>
+}
+
+export type UserDTO = {
+  userId: number,
+  canonicalUserId: string
 }
 
 export class UserStore implements IUserStore {
 
-    constructor(
-        private readonly _pool: Pool
-    ) {}
+  constructor(
+    private readonly _pool: Pool
+  ) { }
 
-    async GetUserIdFromCanonicalUserId(canonicalUserId: string): Promise<string> {
-        // use database_pool here; it's a singleton anyways
-        return "hello, world!";
+  async GetUserFromCanonicalUserId(canonicalUserId: string): Promise<UserDTO> {
+    try {
+      const response = await this._pool.query<UserDTO>(
+`SELECT
+  u.userId,
+  u.canonicalUserId,
+FROM users AS u
+WHERE u.canonicalUserId = $1`,
+        [ canonicalUserId ]);
+
+      const result = response.rows[0];
+
+      if (!result) {
+        throw new Error("CanonicalUserId not found");
+      }
+
+      return result;
+    } catch(err) {
+      logger.error(err);
+      throw err;
     }
+  }
 }
 
 export function CreateUserStore(pool: Pool): IUserStore {
-    return new UserStore(pool);
+  return new UserStore(pool);
 }
