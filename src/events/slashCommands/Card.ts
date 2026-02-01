ed450d82aa5cba2c97e7c83c047756ea6d72e2c5
Happy Spanker <happyspanker@outlook.com>
2026-02-02 00:53:59 +0100

    Add card update handling WIP

--- src/events/slashCommands/Card.ts
+++ src/events/slashCommands/Card.ts
@@ -1,13 +1,14 @@
 import { ChatInputCommandInteraction, MessageFlags, SlashCommandBuilder } from "discord.js";
 import { ISlashCommandHandler } from "./interfaces/ICommandHandler.js";
 import { logger } from "../../logger.js";
+import { EphemeralReply } from "../utils/InteractionHelpers.js";
 
 export const CardSlashCommandBuilder = new SlashCommandBuilder()
   .setName("card")
   .setDescription("Handles singlle card actions")
   .addSubcommand(sub => sub
     .setName("upload")
-    .setDescription("Handled uploaded a new card")
+    .setDescription("Handles uploading a new card")
     .addStringOption(opt => opt
       .setName("visibility")
       .setDescription("What should the visibility of your card be?")
@@ -27,7 +28,37 @@ export const CardSlashCommandBuilder = new SlashCommandBuilder()
     .addAttachmentOption(opt => opt
       .setName("card")
       .setRequired(true)
-      .setDescription("The card (either PNG or JSON) to upload")
+      .setDescription("The card (JSON) to upload")
+    )
+  )
+  .addSubcommand(sub => sub
+    .setName("update")
+    .setDescription("Upload an existing card")
+    .addStringOption(opt => opt
+      .setName("identifier")
+      .setRequired(true)
+      .setDescription("The identifier of the card you wish to update")
+    )
+    .addStringOption(opt => opt
+      .setName("visibility")
+      .setDescription("What should the new visibility be?")
+      .setRequired(false)
+      .addChoices([
+        { name: "Private", value: "private" },
+        { name: "Shared", value: "shared" },
+        { name: "Listed", value: "listed" },
+        { name: "Public", value: "public" },
+      ])
+    )
+    .addStringOption(opt => opt
+      .setName("tagline")
+      .setRequired(false)
+      .setDescription("What should the new tagline be?")
+    )
+    .addAttachmentOption(opt => opt
+      .setName("card")
+      .setRequired(false)
+      .setDescription("What should the actual card be?")
     )
   )
 
@@ -36,9 +67,24 @@ export class CardSlashCommandHandler implements ISlashCommandHandler {
 
   async Handle(interaction: ChatInputCommandInteraction): Promise<void> {
     switch (interaction.options.getSubcommand(true)) {
+      case "update": return this.update(interaction);
       case "upload": return this.upload(interaction);
       default: throw new Error("Invalid subcommand received");
     }
+  }
+  
+  private async update(interaction: ChatInputCommandInteraction): Promise<void> {
+    const identifier = interaction.options.getString("identifier");
+    const visibility = interaction.options.getString("visibility");
+    const tagline = interaction.options.getString("tagline");
+    const card = interaction.options.getAttachment("card");
+
+    if (!visibility && !tagline && !card) {
+      await EphemeralReply(interaction, "Guess we're not updating anything!");
+      return;
+    }
+
+    await EphemeralReply(interaction, "update noop")
   }
 
   private async upload(interaction: ChatInputCommandInteraction): Promise<void> {
@@ -59,9 +105,6 @@ export class CardSlashCommandHandler implements ISlashCommandHandler {
       }
     })
 
-    interaction.reply({
-      content: "upload",
-      flags: MessageFlags.Ephemeral
-    })
+    await EphemeralReply(interaction, "upload noop")
   }
 }
